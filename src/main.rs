4cbf6a5792e6bef74916dae7f9e13b399525a91e
Quinten Krijger <quinten.krijger@ing.com>
2024-11-25 21:01:31 +0100

    extract file reading to lib

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use std::env;
 
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+use aoc::read_lines;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -17,12 +15,4 @@ fn main() {
             println!("{}", line);
         }
     }
-}
-
-// The output is wrapped in a Result to allow matching on errors.
-// Returns an Iterator to the Reader of the lines of the file.
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
 }

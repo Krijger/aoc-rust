cdf61b5b39cfffd4005c6c6d4ecd46c71b002882
Quinten Krijger <quinten.krijger@ing.com>
2024-12-02 11:09:32 +0100

    ex 2024 1 B

--- src/bin/2024-1.rs
+++ src/bin/2024-1.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::env;
 use std::io::Result;
 use std::process::exit;
@@ -12,6 +13,16 @@ fn main() {
     match read_lines(file_path) {
         Ok(lines) => { 
             println!("Answer 1: {}", calculate_1(lines));
+        }
+        Err(e) => {
+            eprintln!("Problem reading file {}: {}", file_path, e);
+            exit(1);
+        }
+    }
+
+    match read_lines(file_path) {
+        Ok(lines) => { 
+            println!("Answer 1: {}", calculate_2(lines));
         }
         Err(e) => {
             eprintln!("Problem reading file {}: {}", file_path, e);
@@ -40,6 +51,26 @@ fn calculate_1(lines: impl Iterator<Item = Result<String>>) -> u32 {
     diff_sum
 }
 
+fn calculate_2(lines: impl Iterator<Item = Result<String>>) -> u32 {
+    let mut counters: HashMap<u32, (u32, u32)> = HashMap::new();
+    for line in lines {
+        let text = line.unwrap();
+        let mut parts = text.split_whitespace();
+        let x: u32 = parts.next().unwrap().parse().unwrap();
+        let y: u32 = parts.next().unwrap().parse().unwrap();
+
+        counters.entry(x).and_modify(|tup| *tup = (tup.0 + 1, tup.1)).or_insert((1, 0));
+        counters.entry(y).and_modify(|tup| *tup = (tup.0, tup.1 + 1)).or_insert((0, 1));
+    }
+
+    let mut score = 0;
+    for (val, (left, right)) in counters {
+        score += val * left * right;
+    }
+
+    score
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -56,6 +87,20 @@ mod tests {
             3   3
         ";
         assert_eq!(calculate_1(io_lines_from(input)), 11);
+        Ok(())
+    }
+
+    #[test]
+    fn test_example_2() -> Result<()> {
+        let input = "
+            3   4
+            4   3
+            2   5
+            1   3
+            3   9
+            3   3
+        ";
+        assert_eq!(calculate_2(io_lines_from(input)), 31);
         Ok(())
     }
 }

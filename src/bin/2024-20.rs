d4cc16e736664cfa9415c9a2ca907e759ac5cfeb
Quinten Krijger <quinten.krijger@ing.com>
2025-01-03 10:59:02 +0100

    Ex 2024 20 A: but takes 18 minutes

--- src/bin/2024-20.rs
+++ src/bin/2024-20.rs
@@ -21,28 +21,67 @@ fn graph_from_map(map: &Mapp<char>) -> Graph<Point> {
     graph
 }
 
-fn calculate(lines: impl Iterator<Item = Result<String, std::io::Error>>) -> usize {
+fn calculate(lines: impl Iterator<Item = Result<String, std::io::Error>>, min_time_saved: usize) -> usize {
     let map = read_map(lines.map(Result::unwrap).collect());
-    map.print();
     let start = &map.find(|x| *x == 'S').unwrap();
     let end = &map.find(|x| *x == 'E').unwrap();
+
     let graph = graph_from_map(&map);
+    
+    // TODO: possible to make this much more efficient for larger `min_time_saved` by analysing the local map
+    // a bit further.
+    // Now that I think of it, a more general approach is to find to shortest distance of the cheat itself, as
+    // it will save time of that shortest distance minus 2. This is true only because the regular race track
+    // is a single path
+    let mut cheats: Vec<(&Point, &Point)> = Vec::new();
+    for n1 in &graph.nodes {
+        for n2 in &graph.nodes {
+            let are_vertically_sandwiching_a_wall = n1.0 == n2.0
+                    && n1.1.abs_diff(n2.1) == 2 
+                    && map.get(n1.0, (n1.1 + n2.1) / 2).is_some_and(|c| *c == '#');
+            let are_horizontally_sandwiching_a_wall = n1.1 == n2.1
+                    && n1.0.abs_diff(n2.0) == 2 
+                    && map.get((n1.0 + n2.0) / 2, n1.1).is_some_and(|c| *c == '#');
+            if (are_vertically_sandwiching_a_wall || are_horizontally_sandwiching_a_wall)
+            && !cheats.contains(&(n2, n1)) {
+                cheats.push((n1, n2));
+            }
+        }
+    }
 
-    println!("Graph has {} nodes", graph.nodes.len());
-
-    let weight = |start: &Point, to: &Point| -> Option<usize> {
-        if (start.0 == to.0 && start.1.abs_diff(to.1) == 1)
-        || (start.1 == to.1 && start.0.abs_diff(to.0) == 1) {
+    let base_weight = |from: &Point, to: &Point| -> Option<usize> {
+        if (from.0 == to.0 && from.1.abs_diff(to.1) == 1)
+        || (from.1 == to.1 && from.0.abs_diff(to.0) == 1) {
             Some(1)
         } else {
             None
         }
     };
 
-    graph.minimum_distance(
+    let benchmark = graph.minimum_distance(
         |p| p.0 == start.0 && p.1 == start.1,
         |p| p.0 == end.0 && p.1 == end.1,
-        weight)
+        base_weight);
+
+    cheats.iter()
+    .enumerate()
+    .inspect(|(i, _)| println!("Processing cheat {} of {}", i, cheats.len()))
+    .map(|(_, cheat)| {
+        graph.minimum_distance(
+            |p| p.0 == start.0 && p.1 == start.1,
+            |p| p.0 == end.0 && p.1 == end.1,
+            |from, to| {
+                if from == cheat.0 && to == cheat.1
+                || from == cheat.1 && to == cheat.0 {
+                    Some(2)
+                } else {
+                    base_weight(from, to)
+                }
+            })
+    })
+    .map(|min_dist| benchmark - min_dist)
+    .filter(|time_saved| *time_saved >= min_time_saved)
+    .count()
 }
 
 fn main() {
@@ -52,7 +91,7 @@ fn main() {
     match aoc::read_lines(file_path) {
         Ok(lines) => { 
             let start = std::time::Instant::now();
-            println!("Answer: {:?}", calculate(lines));
+            println!("Answer: {:?}", calculate(lines, 100));
             println!("Time elapsed in expensive_function() is: {:?}", start.elapsed());
         }
         Err(e) => {
@@ -87,7 +126,9 @@ mod tests {
 
     #[test]
     fn test_regular_shortest_path() -> std::io::Result<()> {
-        assert_eq!(calculate(io_lines_from(INPUT)), 84);
+        assert_eq!(calculate(io_lines_from(INPUT), 0), 44);
+        assert_eq!(calculate(io_lines_from(INPUT), 12), 8);
+        assert_eq!(calculate(io_lines_from(INPUT), 64), 1);
         Ok(())
     }
 }

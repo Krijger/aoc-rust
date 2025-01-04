26d9ccd666ca0e185d5571074a844126c95a3704
Quinten Krijger <quinten.krijger@ing.com>
2025-01-04 10:37:19 +0100

    switch to method that analyses the cheat itself

--- src/bin/2024-20.rs
+++ src/bin/2024-20.rs
@@ -23,26 +23,14 @@ fn graph_from_map(map: &Mapp<char>) -> Graph<Point> {
 
 fn calculate(lines: impl Iterator<Item = Result<String, std::io::Error>>, min_time_saved: usize) -> usize {
     let map = read_map(lines.map(Result::unwrap).collect());
-    let start = &map.find(|x| *x == 'S').unwrap();
-    let end = &map.find(|x| *x == 'E').unwrap();
 
     let graph = graph_from_map(&map);
     
-    // TODO: possible to make this much more efficient for larger `min_time_saved` by analysing the local map
-    // a bit further.
-    // Now that I think of it, a more general approach is to find to shortest distance of the cheat itself, as
-    // it will save time of that shortest distance minus 2. This is true only because the regular race track
-    // is a single path
     let mut cheats: Vec<(&Point, &Point)> = Vec::new();
     for n1 in &graph.nodes {
         for n2 in &graph.nodes {
-            let are_vertically_sandwiching_a_wall = n1.0 == n2.0
-                    && n1.1.abs_diff(n2.1) == 2 
-                    && map.get(n1.0, (n1.1 + n2.1) / 2).is_some_and(|c| *c == '#');
-            let are_horizontally_sandwiching_a_wall = n1.1 == n2.1
-                    && n1.0.abs_diff(n2.0) == 2 
-                    && map.get((n1.0 + n2.0) / 2, n1.1).is_some_and(|c| *c == '#');
-            if (are_vertically_sandwiching_a_wall || are_horizontally_sandwiching_a_wall)
+            let cheat_dist = n1.0.abs_diff(n2.0) + n1.1.abs_diff(n2.1);
+            if cheat_dist == 2
             && !cheats.contains(&(n2, n1)) {
                 cheats.push((n1, n2));
             }
@@ -58,28 +46,28 @@ fn calculate(lines: impl Iterator<Item = Result<String, std::io::Error>>, min_ti
         }
     };
 
-    let benchmark = graph.minimum_distance(
-        |p| p.0 == start.0 && p.1 == start.1,
-        |p| p.0 == end.0 && p.1 == end.1,
-        base_weight);
+    // TODO: allow `graph.minimum_distance` for a subgraph, with a filter for the nodes.
+    // That gives a way to upper limit in our exercise:
+    //      there always is a connection between 2 points (race track property)
+    //      if no connection is found in a subgraph of radius T/2 around the center of the points
+    //      then the min_dist between those points must be > T
+    //      So for T = 100, and points 2 steps apart, take a subgraph of radius 51 and find a connection
+    // Also, allow for a lower limit (carthesian distance in our example), that short cicuits
+    // the distance calculation, because you know even smaller distance is impossible
+    // Also, can Dijkstra be made more efficient in case the end node is known?
 
     cheats.iter()
     .enumerate()
     .inspect(|(i, _)| println!("Processing cheat {} of {}", i, cheats.len()))
     .map(|(_, cheat)| {
         graph.minimum_distance(
-            |p| p.0 == start.0 && p.1 == start.1,
-            |p| p.0 == end.0 && p.1 == end.1,
-            |from, to| {
-                if from == cheat.0 && to == cheat.1
-                || from == cheat.1 && to == cheat.0 {
-                    Some(2)
-                } else {
-                    base_weight(from, to)
-                }
-            })
+            |p| p.0 == cheat.0.0 && p.1 == cheat.0.1,
+            |p| p.0 == cheat.1.0 && p.1 == cheat.1.1,
+            |from, to| base_weight(from, to)
+        )
     })
-    .map(|min_dist| benchmark - min_dist)
+    .inspect(|cheat_dist| println!("Cheat distance: {}", cheat_dist))
+    .map(|cheat_dist| cheat_dist - 2)
     .filter(|time_saved| *time_saved >= min_time_saved)
     .count()
 }
@@ -126,7 +114,7 @@ mod tests {
 
     #[test]
     fn test_regular_shortest_path() -> std::io::Result<()> {
-        assert_eq!(calculate(io_lines_from(INPUT), 0), 44);
+        assert_eq!(calculate(io_lines_from(INPUT), 2), 44);
         assert_eq!(calculate(io_lines_from(INPUT), 12), 8);
         assert_eq!(calculate(io_lines_from(INPUT), 64), 1);
         Ok(())
